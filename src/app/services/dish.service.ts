8d7d5a75523fbabe2280c8d686539868756d23bf
schuang29 <schuang@yahoo.com>
2018-03-16 17:40:26 -0400

    Promise part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,6 @@
+import { DISHES } from './../shared/dishes';
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
 
 @Injectable()
 export class DishService {
@@ -8,15 +8,24 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+    return new Promise(resolve => {
+      //  Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
   getDish(id: number): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+    return new Promise(resolve => {
+      //  Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => (dish.featured))[0]);
+    return new Promise(resolve => {
+      //  Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.featured))[0]), 2000);
+    });
   }
 
 }
