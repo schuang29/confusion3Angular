37c8f1f52dd0b321a71316a3fdaa61ecd66d7fe0
schuang29 <schuang@yahoo.com>
2018-03-22 06:04:08 -0400

    reactive forms part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,8 +12,35 @@ export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
 
-  constructor(private fb: FormBuilder) {
+  validationMessages = {
+    'firstname': {
+      'required': 'First Name is required.',
+      'minlength': 'First Name must be at least 2 characters long',
+      'maxlength': 'First Name cannot be more than 25 characters long',
+    },
+    'lastname': {
+      'required': 'Last Name is required.',
+      'minlength': 'Last Name must be at least 2 characters long',
+      'maxlength': 'Last Name cannot be more than 25 characters long',
+    },
+    'telnum': {
+      'required': 'Tel. Number is required',
+      'pattern': 'Tel. Number must contain only numbers'
+    },
+    'email': {
+      'required': 'Email is required',
+      'email': 'Email not in valid format'
+    }
+  };
+
+  constructor(private fb: FormBuilder) { // Inject Formbuilder
     this.createForm();
    }
 
@@ -22,16 +49,38 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: ['', Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // reset form validation messages
   }
 
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+
+    // tslint:disable-next-line:forin
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control. dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        // tslint:disable-next-line:forin
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
